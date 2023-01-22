128622e507ef261e59e8da5923a05b16313a7f63
ValeryVVV <110850242+ValeryVVV@users.noreply.github.com>
2023-01-22 16:56:54 +0200

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const bodyEl = document.querySelector('body');
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
 
+
+stopEl.disabled = true;
+let interval = null;
+
+const colorGenerator = {
+  DELAY: 100,
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  interval() {
+    interval = setInterval(() => {
+        changeColor();
+    }, this.DELAY);
+    stopEl.disabled = false;
+  },
+
+  start() {
+    startEl.addEventListener('click', () => {
+      this.interval();
+      startEl.disabled = true;
+      stopEl.disabled = false;
+    });
+    stopEl.addEventListener('click', this.stop);
+  },
+
+  stop() {
+    clearInterval(interval);
+    stopEl.disabled = true;
+    startEl.disabled = false;
+  },
+};
+
+function changeColor() {
+  bodyEl.style.backgroundColor = `${colorGenerator.getRandomHexColor()}`;
+}
+
+colorGenerator.start();
